0468d0cfc6e0a9e027a6b0e8431ae4b81bd0a97e
Patrick Henegan <phenegan03@gmail.com>
2022-03-03 13:09:45 -0500

    changed enemy collision, added camera, a few other things from tutorial

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -2,7 +2,19 @@ extends Actor
 
 # This function is run on initialization
 func _ready() -> void:
-	_velocity.x = speed.x
+	set_physics_process(false)
+	_velocity.x = -speed.x
+
+# Called a callback function - typically placed right under the ready function
+# Whenever something enters the stomp hitbox, this script is called.
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+	if (body.global_position.y > get_node("StompDetector").global_position.y):
+		return
+	else:
+		get_node("CollisionShape2D").disabled = true
+		queue_free()
+
+
 
 func _physics_process(delta: float) -> void:
 	if (is_on_wall()):
